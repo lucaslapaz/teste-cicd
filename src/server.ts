26bb8ed1b6d15e9ef3f29dcd23d434bfa5256e3e
Lucas Lapaz <rawdata.cheater@gmail.com>
2025-06-23 12:32:34 -0300

    qualquer coisa

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import {app} from "./app";
 
-const port = 8099;
+const port = 8033;
 
 app.listen(port, '0.0.0.0', () => {
     console.log(`Servidor rodando na porta ${port}. http://localhost:${port}/`);
