98bb12762a1400d5a3281a382e7253cec585dcb7
Lucas Lapaz <rawdata.cheater@gmail.com>
2025-06-23 04:22:49 -0300

    functional

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express, { NextFunction, Request, Response } from "express";
+
+
+const app = express();
+const port = 8099;
+
+app.use((req:Request, res:Response, next:NextFunction)=> {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+})
+
+app.get("/", (req:Request, res:Response) => {
+    res.status(200).send("Carregou com sucesso!");
+    return;
+})
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Servidor rodando na porta ${port}. http://localhost:${port}/`);
+});
